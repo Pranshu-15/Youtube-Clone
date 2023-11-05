acf50a5b57545ec125feb73eff0ed146aab7329d
Pranshu-15 <pranshu923@gmail.com>
2023-11-05 17:22:16 +0530

    commit

--- js/videoDetails.js
+++ js/videoDetails.js
@@ -11,28 +11,4 @@ function playVideo(data) {
 }
 playVideo(data);
 
-let sidebarOpen = true; // Start with the sidebar closed by default
-const sidebar = document.getElementById("sidebar");
-const play = document.getElementById("play");
 
-// Function to open or close the sidebar and adjust the play's grid-template-columns
-function toggleSidebar() {
-    sidebarOpen = !sidebarOpen;
-
-    if (sidebarOpen) {
-        sidebar.style.width = "17%";
-        play.style.marginLeft = "18.5%";
-        play.style.gridTemplateColumns = "repeat(3, 1fr)";
-    } else {
-        sidebar.style.width = "0";
-        play.style.marginLeft = "0";
-        play.style.gridTemplateColumns = "repeat(4, 1fr)";
-    }
-
-    // Adjust play's width to occupy the full screen when the sidebar is closed
-    // if (!sidebarOpen) {
-    //     play.style.width = "100%";
-    // } else {
-    //     play.style.width = "80%";
-    // }
-}
